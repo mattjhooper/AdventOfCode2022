0498bd5365e4e138756fc42eb77afda6ba491565
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-02 12:11:03 +0000

    Add project files.

---
+++ AdventOfCode2022_Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+
+Console.WriteLine("--- Day 1: Calorie Counting ---");
+
+string[] lines = File.ReadAllLines(@"Input.txt");
+
+long topThreeTotal = 0;
+
+long currentTotal = 0;
+
+List<long> totals = new List<long>();
+
+foreach (string line in lines)
+{
+    if (line == "")
+    {
+        totals.Add(currentTotal);
+        currentTotal = 0;
+    }
+    else
+    {
+        currentTotal += long.Parse(line);
+    }    
+}
+
+totals.Add(currentTotal);
+
+topThreeTotal = totals.OrderByDescending(x => x).Take(3).Sum();
+
+Console.WriteLine(topThreeTotal);
---
+++ AdventOfCode2022_Day2/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("---Day 2: Rock Paper Scissors-- - ");
+
+string[] lines = File.ReadAllLines(@"Input.txt");
+
+
+int totalScore = 0;
+
+/*
+foreach (string line in lines)
+{
+    var hands = line.Split(' ');
+
+    char opponent = hands[0][0];
+    char player = hands[1][0];
+
+    // Console.WriteLine($"Opponent plays {opponent}, I play {player}.");
+
+    //int handscore = ShapeScore(player);
+    //handscore += WinLossDraw(opponent, player);
+    //totalScore += handscore;
+    totalScore += Strategy(opponent, player);
+}
+*/
+
+totalScore = lines.Select(l => CheckHand(GetHands(l))).Sum();
+/*
+totalScore = lines
+    .Select(l => GetHands(l))
+    .Select(p => CheckHand(p))
+    .Sum();
+*/
+
+Console.WriteLine($"Total Score {totalScore}."); // 12111
+
+static (char opponent, char goal) GetHands(string handsAsString)
+{
+    var hands = handsAsString.Split(' ');
+    return (hands[0][0], hands[1][0]);
+}
+
+static int ShapeScore(char shape) => shape switch
+{
+    'X' => 1,
+    'Y' => 2,
+    'Z' => 3,
+    _ => throw new ArgumentOutOfRangeException(nameof(shape), $"Unexpected shape value: {shape}"),
+};
+
+static int WinLossDraw(char opponent, char player) => (opponent, player) switch
+{
+    // A = Rock, B = Paper, C = Scissors
+    // X = Rock, Y = Paper, Z = Scissors
+    // Win = 6, Draw = 3, Loss = 0
+
+    ('A', 'X') => 3,
+    ('A', 'Y') => 6,
+    ('A', 'Z') => 0,
+    ('B', 'X') => 0,
+    ('B', 'Y') => 3,
+    ('B', 'Z') => 6,
+    ('C', 'X') => 6,
+    ('C', 'Y') => 0,
+    ('C', 'Z') => 3,
+    _ => throw new ArgumentOutOfRangeException($"Unexpected pair supplied: {opponent} {player}"),
+};
+
+static int Strategy(char opponent, char goal) => (opponent, goal) switch
+{
+    // A = Rock, B = Paper, C = Scissors
+    // X = Rock, Y = Paper, Z = Scissors
+    // X = lose, Y = Draw, Z = Win
+    // Win = 6, Draw = 3, Loss = 0
+
+    ('A', 'X') => 0 + ShapeScore('Z'),
+    ('A', 'Y') => 3 + ShapeScore('X'),
+    ('A', 'Z') => 6 + ShapeScore('Y'),
+    ('B', 'X') => 0 + ShapeScore('X'),
+    ('B', 'Y') => 3 + ShapeScore('Y'),
+    ('B', 'Z') => 6 + ShapeScore('Z'),
+    ('C', 'X') => 0 + ShapeScore('Y'),
+    ('C', 'Y') => 3 + ShapeScore('Z'),
+    ('C', 'Z') => 6 + ShapeScore('X'),
+    _ => throw new ArgumentOutOfRangeException($"Unexpected pair supplied: {opponent} {goal}"),
+};
+
+static int CheckHand((char opponent, char goal) hands) => Strategy(hands.opponent, hands.goal);
