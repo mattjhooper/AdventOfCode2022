49b6f0526821a98bf23de71985f80b9d937c2e50
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-06 09:18:20 +0000

    Add a static function to get a section

--- AdventOfCode2022_Day6/Program.cs
+++ AdventOfCode2022_Day6/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
 Console.WriteLine("--- Day 6: Tuning Trouble ---");
 
 string stream = await File.ReadAllTextAsync(@"Input.txt");
@@ -16,7 +18,7 @@ while (!found)
 Console.WriteLine($"start-of-packet marker: {p + MARKER_LENGTH - 1}");
 
 int i = 0;
-var position = stream.Select(s => new { i = i, chunk = stream.Substring(i++, MARKER_LENGTH) }).First(c => c.chunk.Distinct().Count() == MARKER_LENGTH).i;
+var position = stream.Select(_ => GetSection(stream, i++, MARKER_LENGTH)).First(c => c.section.Distinct().Count() == MARKER_LENGTH).i;
 
 Console.WriteLine($"start-of-packet marker 2: {position + MARKER_LENGTH}");
 
@@ -26,3 +28,5 @@ foreach(var section in sections)
 */
 
 // 3425
+
+static (int i, string section) GetSection(string stream, int start, int length) => (start, stream.Substring(start, length));
