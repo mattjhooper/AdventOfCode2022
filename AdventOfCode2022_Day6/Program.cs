2b587da217e8f74f625c79d5a47ef32d7287f44b
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-06 09:12:34 +0000

    variations that don't depend on looping

--- AdventOfCode2022_Day6/Program.cs
+++ AdventOfCode2022_Day6/Program.cs
@@ -3,23 +3,26 @@ Console.WriteLine("--- Day 6: Tuning Trouble ---");
 
 string stream = await File.ReadAllTextAsync(@"Input.txt");
 
+const int MARKER_LENGTH = 4;
+
 int p = 0;
 bool found = false;
-var checkMarker = new Queue<char>();
 
-while (!found && p < stream.Length)
+while (!found)
 {
-    checkMarker.Enqueue(stream[p++]);
-
-    if (checkMarker.Count() == 4)
-    {
-        if (checkMarker.Distinct().Count() == 4)
-        {
-            found = true;
-        }
-        checkMarker.Dequeue();
-    }
-
+    found = stream.Skip(p++).Take(MARKER_LENGTH).Distinct().Count() == MARKER_LENGTH;
 }
 
-Console.WriteLine($"start-of-packet marker: {p}");
+Console.WriteLine($"start-of-packet marker: {p + MARKER_LENGTH - 1}");
+
+int i = 0;
+var position = stream.Select(s => new { i = i, chunk = stream.Substring(i++, MARKER_LENGTH) }).First(c => c.chunk.Distinct().Count() == MARKER_LENGTH).i;
+
+Console.WriteLine($"start-of-packet marker 2: {position + MARKER_LENGTH}");
+
+/*
+foreach(var section in sections)
+    Console.WriteLine($"Starter: {section.i}. chunk: {section.chunk}");
+*/
+
+// 3425
