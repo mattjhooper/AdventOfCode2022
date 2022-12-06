611b679dde66b6e9317c981b4b42be68563ed694
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-06 07:55:00 +0000

    Example working

--- AdventOfCode2022_Day6/Program.cs
+++ AdventOfCode2022_Day6/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("--- Day 6: Tuning Trouble ---");
+
+string stream = await File.ReadAllTextAsync(@"Input.txt");
+
+int p = 0;
+bool found = false;
+var checkMarker = new Queue<char>();
+
+while (!found && p < stream.Length)
+{
+    checkMarker.Enqueue(stream[p++]);
+
+    if (checkMarker.Count() == 4)
+    {
+        if (checkMarker.Distinct().Count() == 4)
+        {
+            found = true;
+        }
+        checkMarker.Dequeue();
+    }
+
+}
+
+Console.WriteLine($"start-of-packet marker: {p}");
