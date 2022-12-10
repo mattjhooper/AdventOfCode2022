471798ff6a6abd8ad9dee961a8536e36c831b692
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-10 08:32:28 +0000

    Another go at Step 2 example

--- AdventOfCode2022_Day9/Program.cs
+++ AdventOfCode2022_Day9/Program.cs
@@ -42,12 +42,28 @@ foreach (var move in moves)
 
     for (int k = 1; k<10; k++)
     {
-        rope[k] = Adjust(rope[k-1], rope[k]);
+        if (Math.Abs(rope[8].X - rope[9].X) > 3 || Math.Abs(rope[8].Y - rope[9].Y) > 3)
+        {
+            Print2(rope);
+            Console.WriteLine($"Head: [{rope[8].X},{rope[8].Y}]. Tail [{rope[9].X},{rope[9].Y}]");
+        }
+
+        /*
+        foreach(var adjustment in Adjust2(rope[k - 1], rope[k]))
+        {
+            rope[k] = adjustment;
+            visited2.Add(rope[9]);
+            // Print2(rope);
+        }
+        */
+        rope[k] = Adjust3(rope[k - 1], rope[k]);
+
     }
     visited2.Add(rope[9]);
+    Print2(rope);
     // Print(head, tail);
 }
-Print(rope[0], rope[9], visited2);
+//Print(rope[0], rope[9], visited2);
 Console.WriteLine($"Tail visited {visited2.Count} points.");
 
 static void Print(Point head, Point tail, HashSet<Point>? visited = null)
@@ -87,15 +103,47 @@ static void Print(Point head, Point tail, HashSet<Point>? visited = null)
     Thread.Sleep(150);
 }
 
+static void Print2(List<Point> rope)
+{
+    Console.WriteLine();
+    for (int y = 5; y >= 0; y--)
+    {
+        for (int x = 0; x < 6; x++)
+        {
+            var pos = new Point(x, y);
 
+            if (rope[0] == pos)
+            {
+                Console.Write('H');
+            }
+            else if (rope.Contains(pos))
+            {
+                for (int r = 1; r < rope.Count; r++)
+                {
+                    if (rope[r] == pos)
+                    {
+                        Console.Write(r);
+                        goto skipTo;
+                    }
+                }
+            skipTo:;
+            }
+            else
+            {
+                Console.Write('.');
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 static class Directions
 {
     static readonly Point Up = new Point(0, 1);
     static readonly Point Down = new Point(0, -1);
     static readonly Point Left = new Point(-1, 0);
-    static readonly Point Right = new Point(1, 0);
-    static readonly Point Stay = new Point(0, 0);
+    static readonly Point Right = new Point(1, 0);    
 
 
     public static IEnumerable<Point> GetDirections(string line)
@@ -111,6 +159,8 @@ static class Directions
     public static Point Move(this Point p, Point direction) => new Point (p.X + direction.X, p.Y + direction.Y);
 
     public static Point Delta(Point a, Point b) => new Point(a.X - b.X, a.Y - b.Y);
+
+    public static bool Adjacent(Point a, Point b) => Math.Abs(a.X - b.X) <= 1 && Math.Abs(a.Y - b.Y) <= 1;
 
     public static Point Adjust(Point head, Point tail)
     {
@@ -128,6 +178,63 @@ static class Directions
 
         return tail;
 
+    }
+
+    public static Point Adjust3(Point head, Point tail)
+    {
+        int horizontalAdjustment = GetAdjustment(head.X, tail.X);
+        int verticalAdjustment = GetAdjustment(head.Y, tail.Y);
+
+
+        if (head.X - tail.X > 1)
+            return head.Move(Left) with { Y = head.Y + verticalAdjustment };
+
+        if (head.X - tail.X < -1)
+            return head.Move(Right) with { Y = head.Y + verticalAdjustment };
+
+        if (head.Y - tail.Y > 1)
+            return head.Move(Down) with { X = head.X + horizontalAdjustment };
+
+        if (head.Y - tail.Y < -1)
+            return head.Move(Up) with { X = head.X + horizontalAdjustment };
+
+        return tail;
+
+    }
+
+    public static int GetAdjustment(int v1, int v2) => (v1 - v2) switch
+    {
+        > 1 => -1,
+        < -1 => 1,
+        _ => 0,
+    };
+
+    public static Point GetNewPosition(Point a, Point b)
+    {
+        if (a.X - b.X > 1)
+            return b.Move(Right) with { Y = a.Y };
+
+        if (a.X - b.X < -1)
+            return b.Move(Left) with { Y = a.Y };
+
+        if (a.Y - b.Y > 1)
+            return b.Move(Up) with { X = a.X };
+
+        if (a.Y - b.Y < -1)
+            return b.Move(Down) with { X = a.X };
+
+        return b;
+    }
+
+    public static IEnumerable<Point> Adjust2(Point head, Point tail)
+    {
+        var pos = tail;
+        while (!Adjacent(head, pos))
+        {
+            pos = GetNewPosition(head, pos);
+            yield return pos;
+        }
+        yield return pos;
     }
 
     static Point GetDirection(char c) => c switch
