16c336a33a1a2dbe3d0561cac7c2a25dc5315ce0
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-10 17:07:50 +0000

    Part 1 working

--- AdventOfCode2022_Day10/Program.cs
+++ AdventOfCode2022_Day10/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("--- Day 10: Cathode-Ray Tube ---");
+
+var lines = File.ReadAllLines("Input.txt");
+
+var cycle = 0;
+var x = 1;
+int signalStrength = 0;
+int checkCycle = 20;
+
+foreach (var line in lines)
+{
+    CheckCycle();
+    if (line != "noop")
+    {
+        CheckCycle();
+        int addX = int.Parse(line.Split(' ')[1]);
+
+        x += addX;
+    }
+}
+
+Console.WriteLine($"Sum of signalStrength: {signalStrength}"); 
+
+void CheckCycle()
+{
+    cycle++;
+    if (cycle == checkCycle)
+    {
+        Console.WriteLine($"Cycle: {cycle}. X: {x}. Signal Strength: {cycle * x}");
+        signalStrength += (cycle * x);
+        checkCycle += 40;
+    }
+}
