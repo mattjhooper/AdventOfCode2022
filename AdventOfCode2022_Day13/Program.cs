678873d5e44d71a1687b5e259ea5873c2c30f8f4
mattjhooper <matt.j.hooper@googlemail.com>
2022-12-14 14:52:42 +0000

    Part 2 example working

--- AdventOfCode2022_Day13/Program.cs
+++ AdventOfCode2022_Day13/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
+using static Utils;
 
 Console.WriteLine("--- Day 13: Distress Signal ---");
 
@@ -24,161 +25,201 @@ while (offset < lines.Length)
         correctPairs.Add(index); 
         // Console.Write($"<----OK");
     }
-    Console.WriteLine();
+    // Console.WriteLine();
 
     index++;
 }
 
 Console.WriteLine($"Sum of the indices: {correctPairs.Sum()}");
 
-static bool IsDigit(char c) => c >= '0' && c <= '9';
+var part2 = lines.Where(l => !string.IsNullOrEmpty(l)).ToList();
+part2.Add("[[2]]");
+part2.Add("[[6]]");
 
-static ListInput UnpackPacket(string packet)
+var unpacked = part2.Select(UnpackPacket).ToList();
+unpacked.Sort(new ListInputComparer());
+
+var asString = unpacked.Select(l => l.ToString()).ToList();
+int i1 = 1 + asString.IndexOf("[[2]]");
+int i2 = 1 + asString.IndexOf("[[6]]");
+
+Console.WriteLine($"Key 1: {i1}. Key 2: {i2}. Product: {i1 * i2}");
+
+
+public static class Utils
 {
-    var currList = new ListInput(null);
+    public static bool IsDigit(char c) => c >= '0' && c <= '9';
 
-    int c = 1;
-    while (c < packet.Length)
+    public static ListInput UnpackPacket(string packet)
     {
-        if (packet[c] == '[')
+        var currList = new ListInput(null);
+
+        int c = 1;
+        while (c < packet.Length)
         {
-            var newList = new ListInput(currList);
-            currList.Children.Add(newList);
-            currList = newList;
+            if (packet[c] == '[')
+            {
+                var newList = new ListInput(currList);
+                currList.Children.Add(newList);
+                currList = newList;
+                c++;
+                continue;
+            }
+
+            if (packet[c] == ']')
+            {
+                currList = currList?.Parent ?? currList;
+                c++;
+                continue;
+            }
+
+            if (packet[c] == ',')
+            {
+                c++;
+                continue;
+            }
+
+            var digits = new string(packet.ToCharArray().Skip(c).TakeWhile(IsDigit).ToArray());
+
+            c += digits.Length - 1;
+
+            var val = new IntegerInput(int.Parse(digits), currList);
+            currList.Children.Add(val);
+
             c++;
-            continue;
         }
 
-        if (packet[c] == ']')
+        return currList;
+    }
+
+    public static bool CorrectOrder(IInput left, IInput right)
+    {
+        if (left is ListInput && right is ListInput)
         {
-            currList = currList?.Parent ?? currList;
-            c++;
-            continue;
+            return CorrectOrderList(left as ListInput, right as ListInput);
+        }
+        else if (left is IntegerInput && right is IntegerInput)
+        {
+            return CorrectOrderIntegerInput(left as IntegerInput, right as IntegerInput);
+        }
+        else
+        {
+            return CorrectOrderList(left.AsListInput(), right.AsListInput());
+        }
+    }
+
+    public static bool Equal(IInput left, IInput right)
+    {
+        // Console.WriteLine($"Compare Equal {left} vs {right}.");
+        if (left is IntegerInput && right is IntegerInput)
+        {
+            return EqualIntegerInput(left as IntegerInput, right as IntegerInput);
         }
 
-        if (packet[c] == ',')
+        if (left is ListInput && right is ListInput)
         {
-            c++;
-            continue;
+            return EqualListInput(left as ListInput, right as ListInput);
         }
-
-        var digits = new string(packet.ToCharArray().Skip(c).TakeWhile(IsDigit).ToArray());
-
-        c += digits.Length - 1;
-
-        var val = new IntegerInput(int.Parse(digits), currList);
-        currList.Children.Add(val);
-
-        c++;
-    }
-
-    return currList;
-}
-
-static bool CorrectOrder(IInput left, IInput right)
-{
-    if (left is ListInput && right is ListInput)
-    {
-        return CorrectOrderList(left as ListInput, right as ListInput);
-    }
-    else if (left is IntegerInput && right is IntegerInput)
-    {
-        return CorrectOrderIntegerInput(left as IntegerInput, right as IntegerInput);
-    }
-    else
-    {
-        return CorrectOrderList(left.AsListInput(), right.AsListInput());
-    }
-}
-
-static bool Equal(IInput left, IInput right)
-{
-    Console.WriteLine($"Compare Equal {left} vs {right}.");
-    if (left is IntegerInput && right is IntegerInput)
-    {
-        return EqualIntegerInput(left as IntegerInput, right as IntegerInput);
-    }
-
-    if (left is ListInput && right is ListInput)
-    {
-        return EqualListInput(left as ListInput, right as ListInput);
-    }
-    return false;
-}
-
-static bool CorrectOrderIntegerInput(IntegerInput left, IntegerInput right)
-{
-    Console.WriteLine($"Compare Order {left} vs {right}.");
-    if (left.I < right.I)
-    {
-        Console.WriteLine("Left side is smaller, so inputs are in the right order");
-        return true;
-    }
-    else if (left.I > right.I)
-    {
-        Console.WriteLine("Right side is smaller, so inputs are not in the right order");
         return false;
     }
 
-    Console.WriteLine("Left side is equal to Right side");
-    return true;
-}
-
-
-static bool EqualIntegerInput(IntegerInput left, IntegerInput right) => left.I == right.I;
-
-static bool EqualListInput(ListInput left, ListInput right)
-{
-    bool equal = true;
-    if (left.Children.Count != right.Children.Count) 
+    public static bool CorrectOrderIntegerInput(IntegerInput left, IntegerInput right)
     {
-        equal = false;
-    }
-
-    int i = 0;
-    while (equal && i < left.Children.Count)
-    {
-        equal = Equal(left.Children[i], right.Children[i]);
-        i++;
-    }
-
-    return equal;
-}
-
-static bool CorrectOrderList(ListInput left, ListInput right)
-{
-    Console.WriteLine($"Compare {left} vs {right}.");
-    for (int x = 0; x < Math.Min(left.Children.Count, right.Children.Count); x++)
-    {
-        if (Equal(left.Children[x], right.Children[x]))
+        // Console.WriteLine($"Compare Order {left} vs {right}.");
+        if (left.I < right.I)
         {
-            continue;
+            // Console.WriteLine("Left side is smaller, so inputs are in the right order");
+            return true;
         }
-
-        return CorrectOrder(left.Children[x], right.Children[x]);
-
-        /*
-        if (!CorrectOrder(left.Children[x], right.Children[x]))
+        else if (left.I > right.I)
         {
-            Console.WriteLine("CorrectOrderList, order is not correct.");
+            // Console.WriteLine("Right side is smaller, so inputs are not in the right order");
             return false;
-        } 
-        */
-    }
+        }
 
-    if (left.Children.Count <= right.Children.Count)
-    {
-        Console.WriteLine("Left side ran out of items, so inputs are in the right order");
+        // Console.WriteLine("Left side is equal to Right side");
         return true;
     }
-    else
+
+
+    public static bool EqualIntegerInput(IntegerInput left, IntegerInput right) => left.I == right.I;
+
+    public static bool EqualListInput(ListInput left, ListInput right)
     {
-        Console.WriteLine("Right side ran out of items, so inputs are not in the right order");
-        return false;
+        bool equal = true;
+        if (left.Children.Count != right.Children.Count)
+        {
+            equal = false;
+        }
+
+        int i = 0;
+        while (equal && i < left.Children.Count)
+        {
+            equal = Equal(left.Children[i], right.Children[i]);
+            i++;
+        }
+
+        return equal;
+    }
+
+    public static bool CorrectOrderList(ListInput left, ListInput right)
+    {
+        // Console.WriteLine($"Compare {left} vs {right}.");
+        for (int x = 0; x < Math.Min(left.Children.Count, right.Children.Count); x++)
+        {
+            if (Equal(left.Children[x], right.Children[x]))
+            {
+                continue;
+            }
+
+            return CorrectOrder(left.Children[x], right.Children[x]);
+
+            /*
+            if (!CorrectOrder(left.Children[x], right.Children[x]))
+            {
+                Console.WriteLine("CorrectOrderList, order is not correct.");
+                return false;
+            } 
+            */
+        }
+
+        if (left.Children.Count <= right.Children.Count)
+        {
+            // Console.WriteLine("Left side ran out of items, so inputs are in the right order");
+            return true;
+        }
+        else
+        {
+            // Console.WriteLine("Right side ran out of items, so inputs are not in the right order");
+            return false;
+        }
     }
 }
 
-interface IInput
+public class ListInputComparer : IComparer<ListInput>
+{
+    public int Compare(ListInput? x, ListInput? y)
+    {
+        if (x == null || y == null)
+        {
+            return 0;
+        }
+
+        if (EqualListInput(x, y))
+        {
+            return 0;
+        }
+
+        if (CorrectOrderList(x, y))
+        {
+            return -1;
+        }
+
+        return 1;
+    }
+}
+
+public interface IInput
 {
     ListInput? Parent { get; }
 
@@ -188,7 +229,7 @@ interface IInput
 
 }
 
-class ListInput : IInput
+public class ListInput : IInput
 {
     public ListInput(ListInput? parent)
     {
@@ -212,7 +253,7 @@ class ListInput : IInput
     }
 }
 
-record IntegerInput : IInput
+public record IntegerInput : IInput
 {
     public IntegerInput(int i, ListInput? parent)
     {
